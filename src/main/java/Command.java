408a53732388cb68586f00d79f146de1be113f2c
JiaYuan <hjyadm@gmail.com>
2025-08-28 18:38:31 +0800

    Refactor (command): introduce abstract Command class

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,35 +1,9 @@
-public enum Command {
-    TODO("todo"),
-    DEADLINE("deadline"),
-    EVENT("event"),
-    MARK("mark"),
-    UNMARK("unmark"),
-    DELETE("delete"),
-    LIST("list"),
-    BYE("bye"),
-    UNKNOWN("");
+public abstract class Command {
+    // execute command against current state
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws YinException;
 
-    public final String word;
-
-    Command(String word) {
-        this.word = word;
-    }
-
-    // extract command word from whole command string given by user
-    public static Command getCommand(String command) {
-        if (command == null || command.isEmpty()) {
-            return UNKNOWN;
-        }
-        /* find first space, if no space commandName == whole command
-        else commandName == from index = 0 to index of first space
-        scan through list of command enums and find match */
-        int spaceIndex = command.indexOf(' ');
-        String commandName = ((spaceIndex == -1) ? command : command.substring(0, spaceIndex)).toLowerCase();
-        for (Command c : Command.values()) {
-            if (!c.word.isEmpty() && c.word.equals(commandName)) {
-                return c;
-            }
-        }
-        return UNKNOWN;
+    // whether commadn should exit program after execution or not
+    public boolean isExit() {
+        return false;
     }
 }
